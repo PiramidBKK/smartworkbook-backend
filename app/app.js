e9e9c07467226c4cbe2025fe9b72f18e501e7ff2
PiramidBKK <actuallypiramid@gmail.com>
2023-09-07 15:33:46 +0700

    G

--- app/app.js
+++ app/app.js
@@ -2,13 +2,13 @@ import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import dbConnect from "../config/dbConnect.js";
-import userRoutes from "../routes/usersRoute.js";
+import userRoutes from "../routes/usersRoutes.js";
 import { gobalErrHandler, notFound } from "../middlewares/gobalErrHandler.js";
-import configRoutes from "../routes/configRoute.js";
-import dvdesignRoutes from "../routes/dvdesignRoute.js";
-import dvloginRoutes from "../routes/dvloginRoute.js";
-import swdetailRoutes from "../routes/swdetailRoute.js";
-import swinterfaceRoutes from "../routes/swinterfaceRoute.js";
+import configRoutes from "../routes/configRoutes.js";
+import dvdesignRoutes from "../routes/dvdesignRoutes.js";
+import dvloginRoutes from "../routes/dvloginRoutes.js";
+import swdetailRoutes from "../routes/swdetailRoutes.js";
+import swinterfaceRoutes from "../routes/swinterfaceRoutes.js";
 import cors from 'cors';
 
 //dbConnect
@@ -16,12 +16,13 @@ dbConnect();
 
 const app = express();
 app.use(cors());
+
 //pass incoming data
 app.use(express.json());
 
 //routes
 app.use('/api/v1/users/', userRoutes);
-app.use('/api/v1/workbook/', configRoutes);
+app.use('/api/v1/config/', configRoutes);
 app.use('/api/v1/dvdesign/', dvdesignRoutes);
 app.use('/api/v1/dvlogin/', dvloginRoutes);
 app.use('/api/v1/swdetail/', swdetailRoutes);

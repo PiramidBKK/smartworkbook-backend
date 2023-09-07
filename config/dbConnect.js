997977d92a40ed63d7fbe648dac64bd824601b57
PiramidBKK <actuallypiramid@gmail.com>
2023-09-07 11:41:21 +0700

    A

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const dbConnect = async () =>{
     try{
-        const connected = await mongoose.connect(process.env.MONGO_URL);
+        const connected = await mongoose.createConnection(process.env.MONGO_URL);
         mongoose.set("strictQuery", true);
         console.log(`Mongodb connected ${(await connected).connection.host}`);
         

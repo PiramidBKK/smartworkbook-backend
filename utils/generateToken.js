15679e8a40a5b95ba3e89774a757b55ec41f6697
ratchapon-p <ratchapon-p@rmutp.ac.th>
2023-10-09 23:14:22 +0700

    token-expired

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,7 +1,9 @@
 import jwt from "jsonwebtoken";
 
 const generateToken = (id)=>{
-return jwt.sign({id}, process.env.JWT_KEY , {expiresIn: '3d'});
+return jwt.sign({id}, process.env.JWT_KEY , 
+    // {expiresIn: '3d'}
+    );
 };
 
 export default generateToken;

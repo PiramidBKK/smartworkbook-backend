a51c3eb29cb38f3c65c113193d76ecc6ca61b958
ratchapon-p <ratchapon-p@rmutp.ac.th>
2023-10-10 11:56:46 +0700

    multerDvdesign

--- routes/dvdesignRouter.js
+++ routes/dvdesignRouter.js
@@ -1,10 +1,13 @@
 import { createDvdesign, deleteDvdesign,getoneDvdesign ,getAllDvdesign, updateDvdesign } from "../controllers/dvdesignCtrl.js";
 import express from "express";
 import { isLoggedIn } from "../middlewares/isLoggedIn.js";
+import multer from "multer";
 
 const dvdesignRouter = express.Router();
 
-dvdesignRouter.post("/addnew/:configID",isLoggedIn ,createDvdesign);
+const upload = multer();
+
+dvdesignRouter.post("/addnew/:configID",isLoggedIn,upload.none() ,createDvdesign);
 dvdesignRouter.get("/", getAllDvdesign);
 dvdesignRouter.get("/:id", getoneDvdesign);
 dvdesignRouter.put("/:id", updateDvdesign);
